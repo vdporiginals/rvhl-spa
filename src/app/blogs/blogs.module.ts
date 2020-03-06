e72d4a5891a1e87d62a465b036a09ea039d71227
vdp.originals <vdp.originals@gmail.com>
2020-03-06 16:56:07 +0700

    update blogs demo html

--- src/app/blogs/blogs.module.ts
+++ src/app/blogs/blogs.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BlogSingleComponent } from './blog-single/blog-single.component';
 import { BlogFilterComponent } from './blog-filter/blog-filter.component';
 import { BlogCommentComponent } from './blog-comment/blog-comment.component';
+import { BlogBannerComponent } from './blog-banner/blog-banner.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   imports: [],
   exports: [],
-  declarations: [BlogSingleComponent, BlogFilterComponent, BlogCommentComponent]
+  declarations: [BlogSingleComponent, BlogFilterComponent, BlogCommentComponent, BlogBannerComponent, BlogListComponent]
 })
 export class BlogsModule {}
