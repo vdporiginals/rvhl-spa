2a376ed984db396819d7f859b8b01e52174b79a0
vdp.originals <vdp.originals@gmail.com>
2020-03-24 15:09:31 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
-import { BlogsComponent } from './blogs/blogs.component';
 
 const routes: Routes = [{ path: '', component: HomepageComponent }];
 
