6a5f94d3e531269bc9e922a282061c106cc7de69
vdp.originals <vdp.originals@gmail.com>
2020-05-06 16:33:53 +0700

    update port

--- server.ts
+++ server.ts
@@ -41,7 +41,7 @@ export function app() {
 }
 
 function run() {
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 4500;
 
   // Start up the Node server
   const server = app();
